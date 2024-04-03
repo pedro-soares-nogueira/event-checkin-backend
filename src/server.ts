79f357d5b761fa4cc81d052355a4cc01720526b7
Pedro Soares <dev.pedro.nogueira@gmail.com>
2024-04-03 19:47:41 -0300

    get event route

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-events";
 import { registerforEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 const app = fastify();
 
@@ -13,6 +14,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 app.register(registerforEvent);
+app.register(getEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server is running!");

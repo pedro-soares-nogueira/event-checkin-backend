5f2e3b27fc9897c15bde29fca8bf7f5a5a3d2c6b
Pedro Soares <dev.pedro.nogueira@gmail.com>
2024-04-03 20:43:51 -0300

    get attendees

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
 import { createEvent } from "./routes/create-events";
 import { registerforEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify();
 
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEvent);
 app.register(registerforEvent);
 app.register(getEvent);
+app.register(getAttendeeBadge);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server is running!");
